212d5fd80fc27e9882c6442d610473eac326878d
422UR4H <tiagosindra@id.uff.br>
2023-08-16 00:04:27 -0300

    :construction: feat/signUp

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,7 +1,109 @@
-import React from 'react'
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import useForm from "../Hooks/useForm.js";
+import useToken from "../Hooks/useToken.js";
+import api from "../Services/api.js";
+import Form from "../Components/Atoms/Form.jsx";
+import Input from "../Styles/Input.js";
+import ButtonSubmit from "../Components/Atoms/ButtonSubmit.jsx";
+import SignPagesTemplate from "../Components/Templates/SignPagesTemplate.jsx";
+
+
+function isAnyFieldEmpty({ email, password, user_name, photo }) {
+  return (email === "" || password === "" || user_name === "" || photo === "");
+}
 
 export default function Register() {
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const { login } = useToken();
+  const { form, handleForm } = useForm({
+    email: "",
+    password: "",
+    user_name: "",
+    photo: ""
+  });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (isLoading) return;
+    setIsLoading(true);
+
+    if (isAnyFieldEmpty(form)) {
+      alert("Preencha todos os campos!");
+      return;
+    }
+
+    api.signup(form)
+      .then(({ data }) => {
+        login(data);
+        navigate("/timeline");
+      })
+      .catch((err) => {
+        console.log(err.response);
+        alert(err.response.data.message);
+      })
+      .finally(() => setIsLoading(false));
+  }
+
   return (
-    <div>Register</div>
-  )
+    <SignPagesTemplate>
+      <Form onSubmit={handleSubmit}>
+        <Input
+          name="email"
+          type="email"
+          placeholder="e-mail"
+          value={form.email}
+          onChange={handleForm}
+          maxLength={64}
+          required
+        />
+        <Input
+          name="password"
+          type="password"
+          placeholder="password"
+          value={form.password}
+          onChange={handleForm}
+          minLength={3}
+          maxLength={32}
+          required
+        />
+        {/* <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirmar senha"
+          value={form.confirmPassword}
+          onChange={handleForm}
+          onBlur={validatePassword}
+          ref={confirmInputRef}
+          minLength={3}
+          maxLength={32}
+          required
+        /> */}
+        <Input
+          name="name"
+          type="text"
+          placeholder="username"
+          value={form.user_name}
+          onChange={handleForm}
+          minLength={3}
+          maxLength={64}
+          required
+        />
+        <Input
+          name="photo"
+          type="url"
+          placeholder="pircute url"
+          value={form.photo}
+          onChange={handleForm}
+          required
+        />
+        <ButtonSubmit disabled={isLoading}>
+          Sign Up
+        </ButtonSubmit>
+      </Form>
+      <Link to="/sign-up">Switch back to log in</Link>
+    </SignPagesTemplate>
+  );
 }
