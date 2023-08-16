6268baea8d91b1b3ed9b3cc5ba2e65c20d9f8ad3
Guilherme Oliveira <121904094+GuiOliveiraRamos@users.noreply.github.com>
2023-08-16 20:09:40 -0300

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header";
-import Post from "./Components/Post";
-import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import UserContext from "./Contexts/UserContext";
 import { useEffect, useState } from "react";
@@ -10,27 +8,24 @@ import Register from "./Pages/Register.jsx";
 import Timeline from "./Pages/Timeline.jsx";
 import useToken from "./Hooks/useToken.js";
 
-
 export default function App() {
   const { token } = useToken();
   const [user, setUser] = useState();
 
-  useEffect(()=>{
-
-      console.log(user);
-    
-  },[])
+  useEffect(() => {
+    console.log(user);
+  }, []);
   return (
     <UserContext.Provider value={{ user, setUser }}>
-        <BrowserRouter>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/sign-up" element={<Register />} />
-            <Route path="/timeline" element={<Timeline />} />
-            <Route path="/user/:id" element={<UserPage />} />
-          </Routes>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/sign-up" element={<Register />} />
+          <Route path="/timeline" element={<Timeline />} />
+          <Route path="/user/:id" element={<UserPage />} />
+        </Routes>
+      </BrowserRouter>
     </UserContext.Provider>
   );
 }
